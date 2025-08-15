db401d70da7fd2e66ca74a3e783015e4985a892c
SpringDev-ai <palvikram73@gmail.com>
2025-08-15 15:55:02 +0530

    user entity class updated with feilds

--- src/main/java/com/jspiders/user_app/entity/User.java
+++ src/main/java/com/jspiders/user_app/entity/User.java
@@ -1,5 +1,21 @@
 package com.jspiders.user_app.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int userId;
+	private String userName;
+	private String userEmail;
+	private String userPassword;
+	private String userContact;
+	private String address;
 
 }
